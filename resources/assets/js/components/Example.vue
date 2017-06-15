5723488ce4c3227085e3db6ccd93ce94a0deea16
Juan Andrade <jandrade@umpacto.com>
2017-06-15 10:20:16 -0500

    initial commit

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example component!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        mounted() {
+            axios.get('/categories').then(function (response) {
+                console.log(response.data);
+            }).catch(function (error) {
+                
+            });
+        
+        }
+    }
+</script>
