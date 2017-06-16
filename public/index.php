318679f3c9430acb5ee635cbb259f0bb42a0aa1c
Juan Andrade <jandrade@umpacto.com>
2017-06-16 11:16:37 -0500

    change access control alow origin header

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Origin: http://energizer.dev');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Authorization');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Authorization','X-Requested-With');
 
 /**
  * Laravel - A PHP Framework For Web Artisans
